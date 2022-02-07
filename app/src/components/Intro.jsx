8974c6ad45b4676b28abe9d33814590d3ab4a6bd
Abderraouf <abderraouf.drine99@gmail.com>
2022-02-08 01:03:56 +0300

    Final Changes

--- app/src/components/Intro.jsx
+++ app/src/components/Intro.jsx
@@ -9,6 +9,9 @@ const Container = styled.div`
       height: calc(100vh - 50px);
       display: flex;
       padding: 20px
+      @media only screen and (max-width: 480px){
+        flex-direction: column;
+    }
 `
 const Left = styled.div`
       height: calc(100vh - 50px);
@@ -17,15 +20,28 @@ const Left = styled.div`
       flex-direction: column;
       align-items: center;
       justify-content: center;
+      @media only screen and (max-width: 480px){
+        width:100%;
+        height: 100%;
+    }
 `
 const Title = styled.h1`
     width: 60%
     font-size: 60px;
+    @media only screen and (max-width: 480px){
+        width:100%;
+        height: 100%;
+        font-size: 50px;
+    }
 `
 const Desc = styled.p`
     width: 60%;
     font-size: 20px;
     margin-top: 20px;
+    @media only screen and (max-width: 480px){
+        width:100%;
+        height: 100%;
+    }
 `
 const Info = styled.div`
     width: 60%;
@@ -33,6 +49,11 @@ const Info = styled.div`
     margin: 50px;
     align-items: centre;
     justify-content: space-between;
+    @media only screen and (max-width: 480px){
+        width: 100%;
+        flex-direction: column;
+    }
+    
 `
 const Button = styled.button`
     padding: 15px;
@@ -43,6 +64,10 @@ const Button = styled.button`
     border: none;
     letter-spacing: 2px;
     cursor: pointer;
+    @media only screen and (max-width: 480px){
+       margin-bottom: 20px;
+    }
+    
 `
 const Contact = styled.div`
      display: flex;
@@ -61,6 +86,9 @@ const Right = styled.div`
       height: calc(100vh - 50px);
       width: 40%;
       position: relative;
+      @media only screen and (max-width: 480px){
+        display none;
+    }
 `
 const Image = styled.img`
      width: 100%;
--- app/src/components/Service.jsx
+++ app/src/components/Service.jsx
@@ -1,34 +1,114 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from "styled-components";
 import Woman from "./img/woman.png"
+import MiniCard from './MiniCard';
+import Play from "./img/play.png"
 
 const Container = styled.div`
       display: flex;
+      height: 100%;
+      @media only screen and (max-width: 480px){
+        flex-direction: column;
+    }
+
 `
 const Left = styled.div`
       width: 50%;
+      @media only screen and (max-width: 480px){
+        display: none;
+    }
+
 `
 const Image = styled.img`
       height: 100%;
-      margin-left: 75px;
       position: relative;
-      top: 0px;
+      top: 200px;
+      right: 100px;
+      
+      
       
       
 
 `
 const Right = styled.div`
      width: 50%;
+     @media only screen and (max-width: 480px){
+        width: 100%;
+    }
+
+`
+const Wrapper = styled.div`
+     padding: 50px;
+     display: flex;
+     flex-direction: column;
+     @media only screen and (max-width: 480px){
+        padding: 20px;
+            
+    }
+
+`
+const Title = styled.h1`
+    font-size: 70px;
+`
+const Desc = styled.p`
+    font-size: 20px;
+    color: #555;
+    margin-top: 20px;
+
+`
+const CardContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-top: 50px;
+`
+const Button = styled.button`
+    width: 180px;
+    border: none;
+    border-radius: 10px;
+    background-color: darkblue;
+    color: white;
+    font-size: 20px;
+    padding: 15px;
+    margin-top: 50px;
+    cursor: pointer;
+`
+const Icon = styled.img`
+    width: 20px;
+    margin-right: 10px;
 `
 
 export default function Service() {
+
+      
+
   return <Container>
       <Left>
           <Image src={Woman}/>
+          
+          
 
       </Left>
       <Right>
+            <Wrapper>
+                  <Title>Simple process to start</Title>
+                  <Desc>
+                        We provide digital experience services to startups and small
+                        businesses that are looking for a partner to their digital media, design &
+                        development, lead generation and communication requirements. We work
+                        with you, not for you. Although we have great resources.
+                  </Desc>
+                  <CardContainer>
+                        <MiniCard/>
+                        <MiniCard/>
+                        <MiniCard/>
+                  </CardContainer>
+
+                  <Button><Icon src={Play}/>How it works</Button>
+
+            </Wrapper>
 
       </Right>
+
+      
   </Container>;
 }
